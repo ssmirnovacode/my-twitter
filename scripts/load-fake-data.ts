02c95c862e11152845bd5dc450ebe8e3de590a93
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-10-31 12:43:37 +0100

    Fake posts generating added

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -26,6 +26,21 @@ async function loadFakeData(numUsers: number = 10) {
       );
     }
 
+    const lastCreatedUsers = await client.query(
+      "select id from public.users order by created_at desc limit $1",
+      [numUsers]
+    );
+    console.log(lastCreatedUsers.rows);
+
+    for (const row of lastCreatedUsers.rows) {
+      for (let i = 0; i < Math.ceil(Math.random() * 20); i++) {
+        await client.query(
+          "insert into public.posts (user_id, content) values ($1, $2)",
+          [row.id, faker.lorem.sentence()]
+        );
+      }
+    }
+
     await client.query("commit");
   } catch (error) {
     await client.query("rollback");
