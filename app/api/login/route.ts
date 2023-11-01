d3e3d82e9fe8adf392f836b24c0a8ff6060a29e6
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-11-01 10:55:24 +0100

    jwt enabled with http-cookie

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,7 @@
 import { sql } from "@/db";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
 
 export async function POST(req: Request) {
   const json = await req.json();
@@ -7,5 +9,28 @@ export async function POST(req: Request) {
     "select id, username, password from users where username ilike $1",
     [json.username]
   );
-  return NextResponse.json({ data: res.rows[0] });
+  if (res.rowCount === 0)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  const user = res.rows[0];
+  const match = await bcrypt.compare(json.password, user.password);
+
+  if (!match)
+    return NextResponse.json({ error: "Incorrect password" }, { status: 401 });
+
+  const token = await new SignJWT({})
+    .setProtectedHeader({ alg: "HS256" })
+    .setSubject(user.id)
+    .setIssuedAt()
+    .setExpirationTime("2w")
+    .sign(new TextEncoder().encode("my-jwt-secret"));
+
+  const response = NextResponse.json({ msg: "Login successful" });
+  //preventing cross-site request forgery
+  response.cookies.set("jwt", token, {
+    sameSite: "strict",
+    httpOnly: true,
+    secure: true,
+  });
+
+  return response;
 }
