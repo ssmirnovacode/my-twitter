bfd51ee2b93a3d9d46b090592db4e8c096486dee
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-11-05 12:23:33 +0100

    Navbar refactored and active link styled, Followers, Following and Profile pages scaffolded

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,21 +1,28 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const PATHS = ["/feed", "/profile", "/following", "/followers"];
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const renderLink = (path: string) => {
+    return (
+      <li>
+        <Link
+          href={path}
+          className={pathname.startsWith(path) ? "underline font-bold" : ""}
+        >
+          {path.slice(1).charAt(0).toUpperCase() + path.slice(2)}
+        </Link>
+      </li>
+    );
+  };
+
   return (
     <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg my-2 ">
       <ul className="flex flex-row justify-around w-full">
-        <li>
-          <Link href="/feed">Feed</Link>
-        </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link href="/following">Following</Link>
-        </li>
-        <li>
-          <Link href="/followers">Followers</Link>
-        </li>
+        {PATHS.map((path) => renderLink(path))}
       </ul>
     </nav>
   );
