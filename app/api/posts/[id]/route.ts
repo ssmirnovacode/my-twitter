15d789c4d790f78c7c188db3ae32f6173a7b327a
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-11-07 15:52:21 +0100

    api/posts/:id PATH edit post endpoint added

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -20,3 +20,27 @@ export async function GET(
 
   return NextResponse.json({ data: res.rows[0] });
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: number } }
+) {
+  const body = await req.json();
+  const jwtPayload = await getJWTPayload();
+
+  const res = await sql("select * from posts where id = $1 and user_id = $2", [
+    params.id,
+    jwtPayload?.sub,
+  ]);
+
+  if (res.rowCount === 0)
+    return NextResponse.json({ error: "Not found " }, { status: 404 });
+
+  await sql("update posts set content = $1 where user_id = $2 and id = $3", [
+    body?.content,
+    jwtPayload?.sub,
+    params.id,
+  ]);
+
+  return NextResponse.json({ msg: "update success" }, { status: 201 });
+}
