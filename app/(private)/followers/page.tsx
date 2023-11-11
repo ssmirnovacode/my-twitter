a1d23d0e8959cc09f39a19146354fa857e234e7f
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-11-11 17:17:38 +0100

    Followers page added

--- app/(private)/followers/page.tsx
+++ app/(private)/followers/page.tsx
@@ -1,7 +1,10 @@
+import UsersContainer from "@/app/components/UsersContainer";
+
 export default async function Followers() {
   return (
     <main>
       <h2>Followers</h2>
+      <UsersContainer endpoint="followers" />
     </main>
   );
 }
