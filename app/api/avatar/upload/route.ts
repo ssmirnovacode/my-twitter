b2004976a32958c245a3be0fb3ea728f98a84349
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-11-23 15:12:36 +0100

    TSC error fixed

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get("filename")!;
 
-  const blob = await put(filename, request.body, {
+  const blob = await put(filename, request.body!, {
     access: "public",
   });
 
