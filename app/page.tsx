4a20e94ebcef7f752c0666508bd482f9923b3c97
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-12-07 13:13:20 +0100

    Spinner added to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,27 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import Spinner from "./components/Spinner/Spinner";
 
 export default function Home() {
+  const [loading, setLoading] = useState<undefined | "login" | "signup">();
   return (
     <main className="flex min-h-screen items-center justify-center text-black dark:text-white">
       <div className="flex flex-col gap-2 p-5 max-w-xs w-full dark:bg-slate-800 bg-slate-300">
         <div className="text-center my-4">
-          <h1>My twitter</h1>
+          <h1>WHINE·online</h1>
         </div>
-        <div>
-          <Link href="/signin" className="link">
+        <div onClick={() => setLoading("login")}>
+          <Link href="/signin" className="link justify-center gap-3">
             Sign in
-          </Link>
+            {loading === "login" && <Spinner />}
+          </Link>{" "}
         </div>
-        <div>
-          <Link href="/signup" className="link">
+        <div onClick={() => setLoading("signup")}>
+          <Link href="/signup" className="link justify-center gap-3">
             Sign up
-          </Link>
+            {loading === "signup" && <Spinner />}
+          </Link>{" "}
         </div>
       </div>
     </main>
