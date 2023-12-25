c787dc6edeb24cf14260417b4946713500a85cb7
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-12-25 13:12:25 +0100

    Fixed css import in jest issue

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    "testEnvironment": "jsdom"
+    "testEnvironment": "jsdom",
+    "moduleNameMapper":{
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy" 
+ }
   };
