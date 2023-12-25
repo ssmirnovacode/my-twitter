4f282ff5a0ef4751594884167bf91552baf43313
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-12-25 12:46:18 +0100

    jsdom environment specified

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    "testEnvironment": "jsdom"
   };
