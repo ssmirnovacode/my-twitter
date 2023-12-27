047ad7325ff12b7c565139669808f4dbfdb994e9
ssmirnovacode <svetlana.smirnova213@gmail.com>
2023-12-27 19:29:20 +0100

    Added Spinner for Auth

---
+++ app/components/Auth.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import Spinner from "./Spinner/Spinner";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => setLoading(true);
+  return (
+    <>
+      <Link
+        href="/auth"
+        className="dark:bg-slate-900 bg-slate-400 p-2 rounded-lg w-48 text-center flex justify-center gap-2"
+        onClick={handleClick}
+      >
+        Log in / Register {loading && <Spinner />}
+      </Link>
+      <p className="w-96 mt-3">
+        Please log in or register to be able to view user profiles and have
+        access to more functionality
+      </p>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Post from "@/app/components/Post";
 import { sql } from "@/db";
-import Link from "next/link";
+import Auth from "./components/Auth";
 
 async function getData() {
   const res = await sql(
@@ -16,16 +16,7 @@ export default async function PublicFeed() {
     <main className="my-5 p-2 text-black dark:text-white">
       <h1 className="font-mono text-lg text-center">WHINE·online | Feed</h1>
       <div className="flex items-center flex-col pt-5">
-        <Link
-          href="/auth"
-          className="dark:bg-slate-900 bg-slate-400 p-2 rounded-lg w-48 text-center"
-        >
-          Log in / Register
-        </Link>
-        <p className="w-96 mt-3">
-          Please log in or register to be able to view user profiles and have
-          access to more functionality
-        </p>
+        <Auth />
         <div className="flex flex-col items-center mt-5  dark:bg-slate-800 bg-slate-300 max-w-md  rounded-lg my-2 p-5">
           <h2 className="text-lg font-bold">Recent posts from the community</h2>
           <ul>
